001c9334a2526d24bc30e5043315f19abc742da1
weimengli <782856528@qq.com>
2020-06-03 21:29:06 +0800

    修改

---
+++ pages/login/login.js
@@ -0,0 +1,77 @@
+// pages/login/login.js
+const app = getApp()
+var http = require("../../utils/request.js")
+
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    name:"",
+    password:""
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+  login(){
+    http.post("/users/login",{name:this.data.name,password:parseInt(this.data.password)}).then(res=>{
+     wx.setStorageSync('token', res.data.token)
+    })
+  },
+  setInputVal(e) {
+    app.pulicSetData(e, this)
+  },
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
---
+++ utils/request.js
@@ -0,0 +1,55 @@
+/**
+ * GET请求封装
+ */
+let publicurl="http://192.168.0.232:3000"
+function get(url, data = {}) {
+  return request(url, data, 'GET')
+}
+
+/**
+ * POST请求封装
+ */
+function post(url, data = {}) {
+  return request(url, data, 'POST')
+}
+
+/**
+ * 微信的request
+ */
+function request(url, data = {}, method = "") {
+  var contentType = 'application/json'
+  return new Promise(function(resolve, reject) {
+    wx.request({
+      url: publicurl+url,
+      data: data,
+      method: method,
+      header: {
+        'Content-Type': contentType,
+        'Authorization': 'Bearer ' + wx.getStorageInfoSync('token')
+      },
+      success: function(res) {
+        if (res.data.isSuccess == true) {
+          wx.showToast({
+            title: res.data.msg,
+            icon: 'none',
+          })
+         resolve(res.data)
+        } else{
+          wx.showToast({
+            title: res.data.msg,
+            icon: 'none',
+          })
+        }
+      },
+      fail: function(err) {
+        
+      }
+    })
+  });
+}
+
+module.exports = {
+  request,
+  get,
+  post
+}
